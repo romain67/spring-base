626d17b0b3163254a3b85e69a23fbceda497aaf6
Romain Seignez <seiromain@gmail.com>
2016-09-15 21:42:42 +0200

    Error handlers

--- src/main/java/com/roms/library/controller/advice/HttpErrorHandler.java
+++ src/main/java/com/roms/library/controller/advice/HttpErrorHandler.java
@@ -8,8 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
-public class HttpErrorHandler
-{
+public class HttpErrorHandler {
 
 	@ExceptionHandler(NotFoundException.class)
 	@ResponseStatus(HttpStatus.NOT_FOUND)
--- src/main/java/com/roms/library/controller/advice/ValidationErrorHandler.java
+++ src/main/java/com/roms/library/controller/advice/ValidationErrorHandler.java
@@ -1,6 +1,9 @@
 package com.roms.library.controller.advice;
 
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
@@ -13,8 +16,11 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 
 @ControllerAdvice
-public class ValidationErrorHandler
-{
+public class ValidationErrorHandler {
+
+    @Autowired
+    private MessageSource messageSource;
+
 	private static final String BAD_REQUEST_ERROR_CODE = "invalid_value";
 	private RestErrorMessageDto restErrors;
 
@@ -24,8 +30,11 @@ public class ValidationErrorHandler
 	public RestErrorMessageDto processValidationError(MethodArgumentNotValidException exception) {
 		BindingResult result = exception.getBindingResult();
 		List<FieldError> errors = result.getFieldErrors();
+
+        String message = messageSource.getMessage("error.validation_dto." + result.getObjectName(), null,
+                LocaleContextHolder.getLocale());
 		
-		this.restErrors = new RestErrorMessageDto("Validation of DTO '" + result.getObjectName() + "' has fail", BAD_REQUEST_ERROR_CODE);
+		this.restErrors = new RestErrorMessageDto(message, BAD_REQUEST_ERROR_CODE);
 		this.processError(errors);
 		return this.restErrors;
 	}

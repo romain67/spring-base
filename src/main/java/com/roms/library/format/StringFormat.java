75d9403c669ad36c44798f7559f370b3f6654d3f
Romain Seignez <seiromain@gmail.com>
2016-09-16 13:01:51 +0200

    bugfix StringFormat.canonicalize()

--- src/main/java/com/roms/library/format/StringFormat.java
+++ src/main/java/com/roms/library/format/StringFormat.java
@@ -14,6 +14,15 @@ public class StringFormat {
             value = StringUtils.stripAccents(value);
             value = value.replaceAll("\\P{Alnum}","-");
             value = value.toLowerCase();
+            value = value.replaceAll("-+", "-");
+
+            if (value.length() > 0 && value.substring(value.length() - 1).equals("-")) { // Remove last "-" chars
+                value = value.substring(0, value.length() - 1);
+            }
+
+            if (value.length() > 0 && value.substring(0, 1).equals("-")) { // Remove first "-" chars
+                value = value.substring(1);
+            }
         }
         return value;
     }
